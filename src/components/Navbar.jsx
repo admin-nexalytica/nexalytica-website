f9e76622ddaab258b0d188d4eb1a3826481bfacb
admin-nexalytica <info@nexalytica.com>
2025-03-11 00:50:11 +0000

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
                 <li><a href="#gallery">Gallery</a></li>
                 <li><a href="#blog">Blog</a></li>
                 { /*<li><a href="#team">Team</a></li>*/}
-                {/*<li><a href="#schedule">Schedule</a></li>*/}
+                <li><a href="#schedule">Schedule</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
