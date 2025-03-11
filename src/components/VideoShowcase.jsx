a38db9c0151776765f5aa90be9186eeac63f9b82
Asish Mathew <20723393+kernelguardian@users.noreply.github.com>
2025-03-11 12:53:33 +0000

    Updated video source

--- src/components/VideoShowcase.jsx
+++ src/components/VideoShowcase.jsx
@@ -1,25 +1,29 @@
 // VideoShowcase.jsx
-import React from "react";
-import "./VideoShowcase.css";
+import React from 'react';
+import './VideoShowcase.css';
 
 function VideoShowcase() {
-    return (
-        <section id="video-showcase" className="video-showcase-section">
-            <h2>Watch Nexalytica in Action</h2>
+	return (
+		<section id='video-showcase' className='video-showcase-section'>
+			<h2>Watch Nexalytica in Action</h2>
 
-            <div className="video-container">
-                {/* Using native browser controls */}
-                <video width="560" height="315"  controls >
-                    <source src="./nexa_video.mp4" type="video/mp4" /> 
-                    Your browser does not support the video tag.
-                </video>
-            </div>
+			<div className='video-container'>
+				{/* Using native browser controls */}
+				<video width='560' height='315' controls>
+					<source
+						src='https://pqazc0omc0duhaei.public.blob.vercel-storage.com/nexa_video-a3ZpdVUpsm9PhkCbWJNH60VSYBUg4N.mp4'
+						type='video/mp4'
+					/>
+					Your browser does not support the video tag.
+				</video>
+			</div>
 
-            <p className="video-subtext">
-                Discover how Nexalytica’s advanced analytics can transform your business.
-            </p>
-        </section>
-    );
+			<p className='video-subtext'>
+				Discover how Nexalytica’s advanced analytics can transform your
+				business.
+			</p>
+		</section>
+	);
 }
 
 export default VideoShowcase;
